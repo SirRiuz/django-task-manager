0303dd04380880f6294dabfeb76aef6bba92a365
Mateo Jimenez <nacional21_23@hotmail.com>
2020-12-03 15:18:07 -0500

    Arreglando bug del manager

--- accounts/models.py
+++ accounts/models.py
@@ -39,7 +39,7 @@ class User(AbstractBaseUser , PermissionsMixin):
      todo los datos de lo usuarios
     """
     
-    objects = ''
+    objects = CustomUserManager()
 
     userName = models.CharField(
         max_length=120,
