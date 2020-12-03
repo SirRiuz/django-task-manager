da1f0a14a1ee6e056b31e94c4a5eeb36c2707449
Mateo Jimenez <nacional21_23@hotmail.com>
2020-12-03 14:32:29 -0500

    Creo el modelo de usuario , para implementar mas tarde la autenticacion

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,53 @@
-from django.db import models
 
-# Create your models here.
+# Django
+from django.db import models
+from django.contrib.auth.models import (BaseUserManager,PermissionsMixin,AbstractBaseUser)
+
+
+
+
+class User(AbstractBaseUser , PermissionsMixin):
+
+    """
+     Este modelo se encarga de administrar
+     todo los datos de lo usuarios
+    """
+    
+    objects = ''
+
+    userName = models.CharField(
+        max_length=120,
+        null=False,
+        default='',
+        help_text='Nombre de usuario'
+    )
+
+    nickName = models.CharField(
+        null=False,
+        max_length=50,
+        unique=True,
+        help_text='Nick name'
+    )
+    
+    email = models.EmailField(null=False,unique=True,help_text='Correo electronico')
+    phoneNumber = models.IntegerField(null=True,blank=True,help_text='Numero de telefono')
+
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+
+    USERNAME_FIELD = 'nickName'
+    REQUIRED_FIELDS = [  ]
+
+
+    def __str__(self):
+        return self.nickName
+
+
+
+
+
+
+
+
+
