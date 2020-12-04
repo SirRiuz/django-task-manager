19b88032e5fa1d010952a260910ce59a4c0c6bbb
Mateo Jimenez <nacional21_23@hotmail.com>
2020-12-03 19:26:55 -0500

    Cree el modelo de las tareas

--- task/models.py
+++ task/models.py
@@ -1,3 +1,44 @@
+
+# Django 
 from django.db import models
 
-# Create your models here.
+# Models
+from accounts.models import User
+
+
+class Task(models.Model):
+
+
+    """
+     Este modelo se encarga de gestionar
+     todas las tareas
+    """
+    
+    user = models.OneToOneField(to=User , on_delete=models.CASCADE)
+    
+    title = models.CharField(
+        null=False,default='',
+        max_length=100,
+        help_text='Titulo de la tarea'
+    )
+
+    description = models.TextField(
+        blank=True,
+        null=True,
+        default='',
+        help_text='Descripcion de la tarea'
+    )
+
+    taskStatus = models.CharField(
+        default='nan',
+        max_length=100,
+        null=False,
+        help_text='Estado de la tarea'
+    )
+    
+    isComplete = models.BooleanField(default=False)
+    taskExpire = models.DateField(null=False , help_text='Fecha de expiracion de la tarea')
+    taskCreated = models.DateField(auto_now=True , help_text='Fecha de creacion de la tarea')
+    
+
+
